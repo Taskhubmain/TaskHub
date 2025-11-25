20da47431bd7b8281c2a5a9f0da2d5ad496232d8
admin <taskhubmain@gmail.com>
2025-11-25 23:04:59 +0500

    ImageHostUpdate

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -166,16 +166,16 @@ function Hero() {
               className="grid grid-cols-2 gap-4"
             >
               <div className="aspect-[4/3] rounded-2xl overflow-hidden shadow-lg">
-                <img src="https://i.ibb.co/B23j9r4d/pexels-photo-3184360.jpg" alt="" className="h-full w-full object-cover" />
+                <img src="https://s.iimg.su/s/25/th_gqedYIDxrt9VXzKzj6hd1b4KLEhcHToNhmlMKnqT.jpg" alt="" className="h-full w-full object-cover" />
               </div>
               <div className="aspect-[4/3] rounded-2xl overflow-hidden shadow-lg">
-                <img src="https://i.ibb.co/Kpc92M9b/pexels-photo-3184292.jpg" alt="" className="h-full w-full object-cover" />
+                <img src="https://s.iimg.su/s/25/th_gHOlDERxNEpdXl7l7FvKBviWB2Gf6ZHBt3K28zzM.jpg" alt="" className="h-full w-full object-cover" />
               </div>
               <div className="aspect-[4/3] rounded-2xl overflow-hidden shadow-lg">
-                <img src="https://i.ibb.co/YmY53q6/pexels-photo-3184296.jpg" alt="" className="h-full w-full object-cover" />
+                <img src="https://s.iimg.su/s/25/th_gMvHztDxJwvAaLEc2ziyahbv7cQ9NhTvCgNBzKUt.jpg" alt="" className="h-full w-full object-cover" />
               </div>
               <div className="aspect-[4/3] rounded-2xl overflow-hidden shadow-lg">
-                <img src="https://i.ibb.co/Zpt7pJ2g/pexels-photo-3184465.jpg" alt="" className="h-full w-full object-cover" />
+                <img src="https://s.iimg.su/s/25/th_g2RG8lCx2otSnqdg6vvk1FjmQI6Yz4p7UXL8p1Sd.jpg" alt="" className="h-full w-full object-cover" />
               </div>
             </motion.div>
           </div>
@@ -361,16 +361,16 @@ function About() {
         </div>
         <div className="grid grid-cols-2 gap-4">
           <div className="aspect-[4/3] rounded-2xl overflow-hidden shadow-lg">
-            <img src="https://i.ibb.co/4nzXscL5/pexels-photo-3182773.jpg" alt="" className="h-full w-full object-cover" />
+            <img src="https://s.iimg.su/s/25/th_gernCMixXSjhdc4ehQbm33qcp4PQkFaC7WpTB0rq.jpg" alt="" className="h-full w-full object-cover" />
           </div>
           <div className="aspect-[4/3] rounded-2xl overflow-hidden shadow-lg">
-            <img src="https://i.ibb.co/yFPdZ5Sq/pexels-photo-3184639.jpg" alt="" className="h-full w-full object-cover" />
+            <img src="https://s.iimg.su/s/25/th_gjxjvv8xzhyNB0sAdYiEkKOz6lf82HMbeAvWu19W.jpg" alt="" className="h-full w-full object-cover" />
           </div>
           <div className="aspect-[4/3] rounded-2xl overflow-hidden shadow-lg">
-            <img src="https://i.ibb.co/yF28shsS/pexels-photo-3184357.jpg" alt="" className="h-full w-full object-cover" />
+            <img src="https://s.iimg.su/s/25/th_g5hxx8gxzdg0QRXH0rUoXTG6OyB3tV8WBqblzsk0.jpg" alt="" className="h-full w-full object-cover" />
           </div>
           <div className="aspect-[4/3] rounded-2xl overflow-hidden shadow-lg">
-            <img src="https://i.ibb.co/679GMsD6/pexels-photo-3184325.jpg" alt="" className="h-full w-full object-cover" />
+            <img src="https://s.iimg.su/s/25/th_g49TGDWxY9YMCkgOyAEsoCjrWUaC9CplfCYEtVuC.jpg" alt="" className="h-full w-full object-cover" />
           </div>
         </div>
       </div>
